0a3bcede9146ba1795d190c7cb85e65001ca9698
SimplyRocketMan <steven22work@gmail.com>
2017-09-30 04:07:13 +0200

    Just in case, this is 'upgrated'

--- Basics/lin_regress.py
+++ Basics/lin_regress.py
@@ -3,9 +3,8 @@
 @author: Allan Perez
 """
 import numpy as np
-# import math_functions as mf
-def linearRegress(x,y):
-    return np.dot(x,y)
+from math_functions import linearRegress
+
 
 class LinRegress:
 	def __init__(self):
@@ -15,14 +14,14 @@ class LinRegress:
 	def predict(self, w, x):
 		return linearRegress(w,x) 
 
-	def do(self, xLabels, yLabels, lr=0.001):
+	def do(self, xLabels, yLabels, lr=0.0005):
 		shape = xLabels.shape # mx1
 		frame = np.zeros((shape[0],shape[1]+1))
 		frame[:,1:] = xLabels
 		frame[:,0] = 1
 		self.frameShape = frame.shape # mxn | n=2
 		weights = self._init_weights() # nx1 because I want only one hypothesis 
-		trainer = Train(frame, yLabels, weights, lr)
+		trainer = LinearTrain(frame, yLabels, weights, lr)
 		trainer.training()
 		self.newWeights = trainer.weights # still nx1
 
@@ -32,46 +31,57 @@ class LinRegress:
 		# return np.random.rand(self.frameShape[1], 1)
 		return np.random.rand(self.frameShape[1], 1) / np.sqrt(self.frameShape[1])
 
-class Train(LinRegress):
+class LinearTrain():
 	def __init__(self, xLabels, yLabels, weights, lr):
-		super(LinRegress, self).__init__()
+		# super(LinRegress, self).__init__()
 		self.xLabels = xLabels # mxn
 		self.yLabels = yLabels # mx1
 		self.weights = weights  # nx1 
-		self.hypo = self.predict(self.xLabels, self.weights)
+		self.hypo = linearRegress(self.xLabels, self.weights)
 		self.lr = lr
 
 	def training(self):
 		loss = self._loss()
 		count = 0
 		while np.abs(loss) > 0.0001:
-			if(count == 1000000):
+			if(count == 10000):
 				print("Exceeded")
 				return
-			self.hypo = self.predict(self.xLabels, self.weights)
-
-			alpha = self._loss()
+			self.hypo = linearRegress(self.xLabels, self.weights)
+			# print("Hypothesis: ",self.hypo)
+			# print("yLabels: ",self.yLabels)
+			# print("Subtraciton: ",self.hypo -self.yLabels)
+			# print("Squared: ",(self.hypo -self.yLabels)**2)
+			# print("Summed: ",np.sum((self.hypo -self.yLabels)**2))
+			# print("Loss: ", loss)
+			# print("loss suppoused: ",1/10*np.sum((self.hypo -self.yLabels)**2))
+			alpha = self._loss_der()
+			
 			self.weights = self.weights - self.lr*alpha
-
+			print(self.weights,alpha) 
+	
 			loss = self._loss()
 			count+=1
 
 	def _loss(self):
 		m = len(self.xLabels) # number of points in the set
 		toSumm = (self.hypo - self.yLabels)**2 # jxn - mxn  -> jxm
-		loss = (1/(2*m)) * np.sum(toSumm) # jxm -> jx1 (1x1 if there's 1 hypothesis)
+		loss = (1/(m)) * np.sum(toSumm) # jxm -> jx1 (1x1 if there's 1 hypothesis)
 		return loss
 
 	def _loss_der(self):
 		m = len(self.xLabels) # number of points in the set
-		toSumm = (self.hypo - self.yLabels) * self.xLabels[:,1:]# jxn - mxn  -> mxn * mxn (elementwise) -> mxn
-		loss = (1/(m)) * np.sum(toSumm) # mxn -> mx1 (1x1 if there's 1 hypothesis)
-		print('Sum: ',toSumm.shape)
+		toSumm = (self.hypo - self.yLabels) # jxn - mxn  -> mxn * mxn (elementwise) -> mxn
+		loss = (1/(m)) * np.sum(toSumm* (self.xLabels[:,1:])) # mxn -> mx1 (1x1 if there's 1 hypothesis)
 		return loss
 if __name__ == '__main__':
 	import matplotlib.pyplot as plt
-	A = np.random.rand(10, 1)
-	B = np.random.rand(10, 1)
+	X = np.random.uniform(low=0, high=100, size=10).reshape(10,1)
+	Y = -X + 1 + np.random.normal(scale=10, size=10).reshape(10,1)
 
-	plt.plot(A,B)
+	K = LinRegress()
+	pred = K.do(X,Y)
+
+	plt.plot(X,Y, 'bo')
+	plt.plot(X, pred)
 	plt.show()
